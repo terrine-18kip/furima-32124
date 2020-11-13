3185ef2b71d8a08031fb7b9b36502395b20de41a
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-13 20:25:55 +0900

    テストコードの実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.middle_name}
+    nickname {Faker::Name.name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
-    encrypted_password {password}
-    last_name {Faker::Name.last_name}
-    first_name {Faker::Name.first_name}
+    password_confirmation {password}
+    last_name {"小渕"}
+    first_name {"健太郎"}
     last_name_kana {"ウズマキ"}
     first_name_kana {"ナルト"}
     birthday {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
