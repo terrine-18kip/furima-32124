c7d27d124166eb811565131f34448bdd57f21475
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-13 18:07:44 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  KANA_REGEX = /\A[ァ-ン]+\z/
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
+  validates :password, format: { with: PASSWORD_REGEX }
+  validates :last_name, :first_name, format: { with: NAME_REGEX }
+  validates :last_name_kana, :first_name_kana, format: { with: KANA_REGEX }
 end
