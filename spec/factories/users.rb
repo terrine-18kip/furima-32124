c40220aae48f3161c2bd778671f3a98f47e8cc72
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-13 19:33:21 +0900

    テストコードの雛形の作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,14 @@
-FactoryBot define do
+FactoryBot.define do
   factory :user do
-    nickname {Faker::JapaneseMedia::Naruto.character}
+    nickname {Faker::Name.middle_name}
     email {Faker::Internet.free_email}
     password = Faker::Internet.password(min_length: 6)
     password {password}
     encrypted_password {password}
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
+    last_name_kana {"ウズマキ"}
+    first_name_kana {"ナルト"}
     birthday {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it 'すべての情報が存在すれば登録できること' do
+      expect(@user).to be_valid
+    end
+
+    it 'nicknameが空では登録できないこと' do
+    end
+
+    it 'emailが空では登録できないこと' do
+    end
+
+    it '重複したemailが存在する場合登録できないこと' do
+    end
+
+    it '@を含まないemailは登録できないこと' do
+    end
+
+    it 'passwordが空では登録できないこと' do
+    end
+
+    it 'passwordが6文字以上であれば登録できること' do
+    end
+
+    it 'passwordが5文字以下であれば登録できないこと' do
+    end
+
+    it 'passwordが半角英数字混合でなければ登録できないこと' do
+    end
+
+    it 'passwordとencrypted_passwordが不一致では登録できないこと' do
+    end
+
+    it 'last_nameが空では登録できないこと' do
+    end
+
+    it 'first_nameが空では登録できないこと' do
+    end
+
+    it 'last_nameが全角（漢字・ひらがな・カタカナ）でなければ登録できないこと' do
+    end
+
+    it 'first_nameが全角（漢字・ひらがな・カタカナ）でなければ登録できないこと' do
+    end
+
+    it 'last_name_kanaが空では登録できないこと' do
+    end
+
+    it 'first_name_kanaが空では登録できないこと' do
+    end
+
+    it 'last_name_kanaが全角（カタカナ）でなければ登録できないこと' do
+    end
+
+    it 'first_name_kanaが全角（カタカナ）でなければ登録できないこと' do
+    end
+
+    it 'birthdayが空では登録できないこと' do
+    end
+
+  end
 end
