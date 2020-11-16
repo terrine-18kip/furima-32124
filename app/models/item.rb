49b690752c8111b65be1edfce34e9eb8911252e9
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-16 21:42:16 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :cost
+  belongs_to :area
+  belongs_to :days
+
 end
