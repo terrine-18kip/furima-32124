fc229ad9b7ecd9a2260a91717aada1c317aeee71
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-16 17:52:45 +0900

    new, createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,19 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
-  
+
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :status_id, :ship_cost_id, :ship_area_id, :ship_days_id, :price, :user_id, :image)
   end
 end
