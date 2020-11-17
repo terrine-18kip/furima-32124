b5b0ef43b7c48508cb28ff34a77989894fceb157
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-17 11:30:00 +0900

    ActiveStorageの導入

--- Gemfile
+++ Gemfile
@@ -64,6 +64,8 @@ end
 gem 'devise'
 gem 'pry-rails'
 gem 'active_hash'
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
 
 group :production do
   gem 'rails_12factor'
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
   validates :image,        presence: true
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
