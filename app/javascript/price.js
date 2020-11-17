6f0efe3aeda368fe1997a8743123b8e6c4991197
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-17 12:46:21 +0900

    販売手数料、販売利益の表示

---
+++ app/javascript/price.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function(){
+  const itemPrice = document.getElementById("item-price")
+  
+  itemPrice.addEventListener("keyup", () => {
+    const addTaxPrice = document.getElementById("add-tax-price")
+    const profit = document.getElementById("profit")
+    const taxPrice = itemPrice.value * 0.1
+    const profitPrice = itemPrice.value * 0.9
+    addTaxPrice.innerHTML = taxPrice
+    profit.innerHTML = `${profitPrice}`
+  })
+
+})
