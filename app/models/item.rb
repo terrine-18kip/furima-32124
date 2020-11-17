aca2cc5dfd4d12eb3e34c05132ee19ed783690f7
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-17 11:22:13 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  validates :name,         presence: true, length: { maximum: 6 }
+  validates :explain,      presence: true, length: { maximum: 1000 }
+  validates :category_id,  presence: true
+  validates :status_id,    presence: true
+  validates :ship_cost_id, presence: true
+  validates :ship_area_id, presence: true
+  validates :ship_days_id, presence: true
+  validates :price,        presence: true,
+                           numericality: { greater_than_or_equal_to: 300, less_than: 10000000 }
+  validates :user_id,      presence: true
+  validates :image,        presence: true
+
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
