d7a27d23b5a1c6f851a5469d47bc37e70f12897b
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-18 12:26:14 +0900

    ビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     if item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(item.id)
     else
       @item = Item.find(params[:id])
       render :edit
