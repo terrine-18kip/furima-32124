f581908bce10718e3113bb5b0fd6b589b6f917d2
terrine-18kip <ryota.meiwa.3618@gmail.com>
2020-11-19 12:05:16 +0900

    orderコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'order/index'
+  get 'order/create'
   devise_for :users
   root to: "items#index"
-  resources :items
+  resources :items do
+    resources :order, only: [:index, :create]
+  end
 end
